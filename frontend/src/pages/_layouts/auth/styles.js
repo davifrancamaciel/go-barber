b43708c95cc5ba3daf3ca2e5da359deff239dfb9
Davi França Maciel <davifrancamaciel@hotmail.com>
2021-07-15 09:47:16 -0300

    ajuste de layout

--- frontend/src/pages/_layouts/auth/styles.js
+++ frontend/src/pages/_layouts/auth/styles.js
@@ -2,7 +2,8 @@ import styled from 'styled-components'
 import { darken } from 'polished'
 
 export const Wrapper = styled.div`
-  height: 100%;
+  min-height: 100vh;
+  
   background: linear-gradient(-90deg, #7159c1, #ab59c1);
   display: flex;
   flex-direction: column;
--- frontend/src/pages/_layouts/default/styles.js
+++ frontend/src/pages/_layouts/default/styles.js
@@ -1,7 +1,9 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.div`
-  height: 100%;
+  min-height: 100vh;
+  
+
   background: linear-gradient(-90deg, #7159c1, #ab59c1);  
   padding-bottom: 50px;  
 `
